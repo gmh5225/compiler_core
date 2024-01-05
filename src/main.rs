82111ea878ced93b6a08f6c0004ec94c50313ad4
Caleb L'Italien <caleblitalien@proton.me>
2024-01-05 13:02:50 -0500

    structuring lexer-parser pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub mod frontend;
 pub mod backend;
 
+use frontend::lexer::Lexer;
+use frontend::syntax::token::Token;
+
 use std::io::{self, Write};
 
 fn print_ready() {
@@ -10,19 +13,28 @@ fn print_ready() {
     handle.flush().expect("Error flushing stderr");
 }
 
-fn main_loop() {
-    // Initialization and first token fetch
-    print_ready();
-    // let mut cur_tok = get_next_token(); 
+fn read_user_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.to_string() 
+}
 
-    // // Loop until end of file
-    // loop {
-    //     match cur_tok {
-    //         token::Token::EOF => return,
-    //         ';' => get_next_token(),
-    //         _ => handle_top_level_expression(),
-    //     }
-    // }
+fn main_loop() {
+    // Loop until end of file
+    loop {
+        print_ready();
+        let user_input = read_user_input();
+        if user_input.is_empty() || user_input == "exit" {
+            break;
+        }
+        let tokens: Vec<Token> = Lexer::lex(&user_input);
+        println!("{:?}", tokens);
+        // match cur_tok {
+        //     Token::EOF => return,
+        //     ';' => advance_token(),
+        //     _ => handle_top_level_expression(),
+        // }
+    }
 }
 
 fn main() {
---
+++ target/debug/build/libc-81f9a0448a7133ab/build_script_build-81f9a0448a7133ab.d
@@ -0,0 +1,5 @@
+/Users/caleblitalien/compiler-design/target/debug/build/libc-81f9a0448a7133ab/build_script_build-81f9a0448a7133ab: /Users/caleblitalien/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
+
+/Users/caleblitalien/compiler-design/target/debug/build/libc-81f9a0448a7133ab/build_script_build-81f9a0448a7133ab.d: /Users/caleblitalien/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
+
+/Users/caleblitalien/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs:
